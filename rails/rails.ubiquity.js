2654b9e777d8eb7ae63459c12b60eed30c7ebfae
Michael Bumann <michael@railslove.com>
2008-08-31 14:34:38 +0200

    added rails apidock script

---
+++ rails/rails.ubiquity.js
@@ -0,0 +1,19 @@
+CmdUtils.CreateCommand({
+     name: "rails",
+     takes: {"function": noun_arb_text},
+     homepage: "http://Railslove.com",
+     author: {name: "Michael Bumann", email: "michael@railslove.com"},
+     description: "Search the rails documentation at apidock.com",
+     execute: function(directObject)
+     {
+         var url = "http://apidock.com/rails/search?query="+ directObject.text;
+         Utils.openUrlInBrowser(url);
+     },
+     preview: function(pblock, directObject)
+     {
+         searchText = jQuery.trim(directObject.text);
+         if(searchText.length <= 0)
+         	return;
+         pblock.innerHTML = "Search rails documentation for:" + query;
+     }
+ });
